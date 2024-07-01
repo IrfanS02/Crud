267be7a635905e72d29375a9eeb9dbdbe74f2c22
LABS\placement <sblock>
2024-07-01 19:03:38 +0530

    create the CRUD components

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './Components/Create';
+import Read from './Components/Read';
+import Update from './Components/Update';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Main'>
+      Curd Operation
+      <Create />
+      <Read />
+      <Update/>
     </div>
   );
 }
